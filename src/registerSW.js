4fd9e573f27fbe675686f00ddc9dbee79d76434a
Rafikirestaurante <rafiki.restaurante@gmail.com>
2026-05-17 09:48:31 -0500

    Delete src directory

--- src/registerSW.js
+++
@@ -1,9 +0,0 @@
-export function registerServiceWorker() {
-  if ('serviceWorker' in navigator) {
-    window.addEventListener('load', () => {
-      navigator.serviceWorker
-        .register('/sw.js')
-        .catch((error) => console.warn('No se pudo registrar el service worker:', error));
-    });
-  }
-}
