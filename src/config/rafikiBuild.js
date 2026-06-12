7083a5141e169bba5d47758456fbfd312b39b33f
Rafikirestaurante <rafiki.restaurante@gmail.com>
2026-06-11 19:19:53 -0500

    Delete src directory

--- src/config/rafikiBuild.js
+++
@@ -1,9 +0,0 @@
-export const RAFIKI_BUILD = {
-  version: '119.0-FASE27A-RAFA-TABLAS-MOBILE-2026-06-11',
-  phase: 'Fase 27A - Rafa tablas optimizadas para celular',
-  date: '2026-06-11',
-  fase: '27A',
-  notes: 'Se optimizan las tablas internas de /rafa para celular convirtiéndolas en tarjetas responsivas y evitando desbordes horizontales en Informes, Gastos, Inventario y tablas administrativas.'
-};
-
-export const RAFIKI_APP_VERSION = RAFIKI_BUILD.version;
